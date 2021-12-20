8b493fc7106760d7ab15085983669fb746e50b3f
Cale Nockels <thenockels@gmail.com>
2021-12-20 20:14:20 +0000

    fct

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,19 @@
+WITH
+orders 
+AS
+(SELECT * FROM {{ref('stg_orders')}}),
+
+amount
+AS
+(
+SELECT
+A.order_id,
+A.customer_id,
+B.amount
+FROM 
+orders as A 
+LEFT JOIN 
+`macro-landing-333115.dbt_cnockels.payment` as B on A.order_id = B.orderid
+)
+
+SELECT * FROM amount
