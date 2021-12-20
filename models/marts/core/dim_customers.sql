7472e4e29c3b14ceafa8fa01808920df00a5dd04
Cale Nockels <thenockels@gmail.com>
2021-12-20 20:29:35 +0000

    fct

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -9,17 +9,18 @@ AS
 
 orders 
 AS
-(SELECT * FROM {{ref('stg_orders')}}),
+(SELECT * FROM {{ref('fct_orders')}}),
+
 
 customer_orders 
 AS 
 (
     SELECT 
     customer_id,
-
     min(order_date) as first_order_date,
     max(order_date) as most_recent_order_date,
-    count(order_date) as number_of_orders
+    count(order_id) as number_of_orders,
+    Sum(amount) as lifetime_value
 
     FROM orders
 
@@ -35,12 +36,11 @@ AS
     Customers.LAST_NAME,
     customer_orders.first_order_date,
     customer_orders.most_recent_order_date,
-    coalesce(customer_orders.number_of_orders, 0) as number_of_orders 
-
+    coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+    customer_orders.lifetime_value
+    
     from Customers 
-
-    left join customer_orders using (customer_id)
-
+    left join customer_orders using (customer_id) 
 )
 
 select * from final 
--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -9,6 +9,7 @@ AS
 SELECT
 A.order_id,
 A.customer_id,
+A.order_date,
 B.amount
 FROM 
 orders as A 
